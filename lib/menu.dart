9e87303dff7422ded25436e04284b3ead6534a91
Replicat5 <bilol586746@mail.ru>
2021-11-10 22:05:57 +0300

    test

--- lib/menu.dart
+++ lib/menu.dart
@@ -98,7 +98,7 @@ class RowWidgets extends StatelessWidget {
             ),
             Text(
               data.mail,
-              style: TextStyle(fontSize: 12),
+              style: TextStyle(fontSize: 11),
             ),
           ],
         ),
